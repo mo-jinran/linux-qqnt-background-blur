1e56543bd5ff12c52270f29570d3096582650f49
student_2333 <lgc2333@126.com>
2024-01-29 21:55:59 +0800

    format

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 // 运行在 Electron 主进程 下的插件入口
-const { exec, execSync } = require('child_process');
-const { Module } = require('module');
+const { exec, execSync } = require("child_process");
+const { Module } = require("module");
 
 // Proxy BrowserWindow，设置窗口透明
 const original_load = Module._load;
 Module._load = (...args) => {
     const loaded_module = original_load(...args);
 
-    if (args[0] != 'electron') {
+    if (args[0] != "electron") {
         return loaded_module;
     }
 
@@ -18,7 +18,7 @@ Module._load = (...args) => {
                 [
                     {
                         ...original_config,
-                        backgroundColor: '#00000000',
+                        backgroundColor: "#00000000",
                         transparent: true,
                     },
                 ],
@@ -29,7 +29,7 @@ Module._load = (...args) => {
 
     return new Proxy(loaded_module, {
         get(target, property, receiver) {
-            if (property === 'BrowserWindow') {
+            if (property === "BrowserWindow") {
                 return HookedBrowserWindow;
             }
             return Reflect.get(target, property, receiver);
@@ -42,8 +42,8 @@ function setBackgroundBlur(window_id) {
     // 命令太长，我就给分开了
     const parms = {
         id: `-id ${window_id}`,
-        f: '-f _KDE_NET_WM_BLUR_BEHIND_REGION 32c',
-        set: '-set _KDE_NET_WM_BLUR_BEHIND_REGION 0x0',
+        f: "-f _KDE_NET_WM_BLUR_BEHIND_REGION 32c",
+        set: "-set _KDE_NET_WM_BLUR_BEHIND_REGION 0x0",
     };
     const set_blur_command = `xprop ${parms.id} ${parms.f} ${parms.set}`;
     exec(set_blur_command);
@@ -56,12 +56,12 @@ function getWindowIdArray() {
     try {
         // 执行命令获取QQ的窗口
         const shell_command = `wmctrl -l | grep "QQ"`;
-        const stdout = execSync(shell_command, { encoding: 'utf-8' });
+        const stdout = execSync(shell_command, { encoding: "utf-8" });
         // 如果有多个窗口，先每行分开
-        const lines = stdout.trim().split('\n');
+        const lines = stdout.trim().split("\n");
         for (const line of lines) {
             // 按空格分开，第一个就是窗口id
-            const window_id = line.split(' ')[0];
+            const window_id = line.split(" ")[0];
             window_ids.push(window_id);
         }
     } finally {
@@ -73,15 +73,15 @@ function getWindowIdArray() {
 let prev_ids = [];
 
 function onBrowserWindowCreated(window) {
-    window.once('show', () => {
+    window.once("show", () => {
         // 设置窗口也会设置纯黑，所以需要改透明
-        window.setBackgroundColor('#00000000');
+        window.setBackgroundColor("#00000000");
 
         // 给每个新开的窗口后面都加上QQ
         // 因为我发现有些窗口不带QQ这俩字符
         // 比如设置窗口就叫设置，导致获取不到窗口ID（
         const window_title = window.getTitle();
-        if (!window_title.includes('QQ')) {
+        if (!window_title.includes("QQ")) {
             window.setTitle(`${window_title}QQ`);
         }
 
